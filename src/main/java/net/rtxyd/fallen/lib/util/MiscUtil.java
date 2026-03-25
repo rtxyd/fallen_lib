5960e8969587f51ed4b7067619dc31e2ba709e49
rtxyd <65117312+rtxyd@users.noreply.github.com>
2026-03-26 06:33:26 +0800

    Launcher targets

--- src/main/java/net/rtxyd/fallen/lib/util/MiscUtil.java
+++ src/main/java/net/rtxyd/fallen/lib/util/MiscUtil.java
@@ -1,6 +1,33 @@
 package net.rtxyd.fallen.lib.util;
 
+import java.util.List;
+
 public class MiscUtil {
+    public final static List<String> launcherTargets = List.of(
+            "mcp",
+            "srg",
+            "fmlclientdev",
+            "fmlclient",
+            "fmlclientuserdev",
+            "fmldatauserdev",
+            "fmlserverdev",
+            "fmlserveruserdev",
+            "forgeclientdev",
+            "forgeclient",
+            "forgeclientuserdev",
+            "forgedatadev",
+            "forgedatauserdev",
+            "forgegametestserverdev",
+            "forgegametestserveruserdev",
+            "forgeserverdev",
+            "forgeserver",
+            "forgeserveruserdev"
+    );
+
+    public static List<String> specialLauncherTargets = List.of("mcp", "srg");
+
+    public static List<String> productionLauncherTargets = List.of("fmlclient","forgeclient","forgeserver");
+
     public static String missingModMessage(String currentModId, String missingModId, String missingModVersion) {
         return String.format(                    "§c§l==================================================\n" +
                 "§c§l  REQUIRED MOD '%2$s' IS MISSING!\n" +
